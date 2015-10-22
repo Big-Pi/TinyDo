704be4c6ec169e369341829c0fe901941e289d71
Big-Pi <wangdapishuai@163.com>
2015-10-23 02:29:38 +0800

    setup basic editable cell and go to sleep

--- TinyDo/Controller/ToDoListViewController.m
+++ TinyDo/Controller/ToDoListViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ToDoListViewController.h"
+#import "EditableCell.h"
 
-@interface ToDoListViewController ()
+@interface ToDoListViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
@@ -17,6 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.tableView registerNib:[UINib nibWithNibName:@"EditableCell" bundle:nil] forCellReuseIdentifier:@"Cell"];
 }
 
 /*
@@ -28,5 +30,21 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    EditableCell *cell= [self.tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    cell.textField.placeholder=[NSString stringWithFormat:@"%ld",indexPath.row];
+    cell.frame=CGRectMake(cell.frame.origin.x, cell.frame.origin.y, self.tableView.frame.size.width, 66);
+    return cell;
+}
+
+
+
 
 @end
---
+++ TinyDo/View/EditableCell.m
@@ -0,0 +1,24 @@
+//
+//  EditableCell.m
+//  TinyDo
+//
+//  Created by pi on 15/10/23.
+//  Copyright (c) 2015年 pi. All rights reserved.
+//
+
+#import "EditableCell.h"
+
+@interface EditableCell ()
+
+
+@end
+
+@implementation EditableCell
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
