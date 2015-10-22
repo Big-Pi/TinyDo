77ca6878b24151953e87b8623ee6c6f825e5bf12
Big-Pi <wangdapishuai@163.com>
2015-10-23 01:43:21 +0800

    basic setup

---
+++ TinyDo/Controller/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  TinyDo
+//
+//  Created by pi on 15/10/23.
+//  Copyright (c) 2015年 pi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
