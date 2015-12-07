8c6ea8b98a056edc3720714ade055a6efafe2e16
BigPi <wangdapishuai@163.com>
2015-12-08 00:49:40 +0800

    add lib chemeleon to generate random theme color and little bug fix

---
+++ TinyDo/Tool/UIColor+AllFlatColors.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+AllFlatColors.h
+//  TinyDo
+//
+//  Created by pi on 15/12/6.
+//  Copyright © 2015年 pi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (AllFlatColors)
+
+@end
---
+++ TinyDo/Tool/UIColor+AllFlatColors.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+AllFlatColors.m
+//  TinyDo
+//
+//  Created by pi on 15/12/6.
+//  Copyright © 2015年 pi. All rights reserved.
+//
+
+#import "UIColor+AllFlatColors.h"
+
+@implementation UIColor (AllFlatColors)
+
+@end
